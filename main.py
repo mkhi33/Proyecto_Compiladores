684d5defa2f8462f1982e9c1f710707007b47f1f
Amilcar Rodriguez <mkhi@kali.hn>
2021-11-25 12:17:44 -0600

    Se crean archivos de prueba

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-from nucleo.analizadorLexico import Lexico
 import nucleo.ply.ply.lex as lex;
 import re
 import codecs
@@ -6,48 +5,6 @@ import os
 import sys
 
 
-tokens = [
-            "ID", "NUMERO", "APARENT", "CPARENT", "SUM", "RES", "MULT", "DIV", "POT", "IGUAL", "PUNTO"
-         ]
-        
-t_APARENT = r'\('
-t_CPARENT = r'\('
-t_SUM = r'\+'
-t_RES = r'-'
-t_MULT = r'\*'   
-t_DIV = r'/'
-t_POT = r'\^' 
-t_IGUAL = r'='
-t_PUNTO = r'\.'
-t_ID = t_ID  = r'[a-zA-Z_][a-zA-Z0-9_]*'
+from nucleo.testAnalizadorLexico import *
 
-def t_NUMERO( t ):
-    r'\d+'
-    try:
-        t.value = int(t.value)
-    except ValueError:
-        print("Integer value too large %d", t.value)
-        t.value = 0
-    return t
-
-def t_error( t ):
-    print("Caracter ilegal '%s'" % t.value[0])
-    t.lexer.skip(1)
-
-
-
-analizador = lex.lex()
-
-test = 'test/prueba1.txt'
-
-fp = codecs.open(test, "r", "utf-8")
-cadena = fp.read()
-fp.close()
-
-analizador.input(cadena)
-
-
-while True:
-    token = analizador.token()
-    if not token: break
-    print(token)
+test()
--- nucleo/analizadorLexico.py
+++ nucleo/analizadorLexico.py
@@ -1,57 +1,34 @@
-import nucleo.ply.ply.lex as lex;
-import re
-import codecs
-import os
-import sys
 
-class Lexico:
-    def __init__(self):
-        self.tokens = [
+
+tokens = [
             "ID", "NUMERO", "APARENT", "CPARENT", "SUM", "RES", "MULT", "DIV", "POT", "IGUAL", "PUNTO"
          ]
         
-        self.t_APARENT = r'\('
-        self.t_CPARENT = r'\('
-        self.t_SUM = r'\+'
-        self.t_RES = r'-'
-        self.t_MULT = r'\*'   
-        self.t_DIV = r'/'
-        self.t_POT = r'\^' 
-        self.t_IGUAL = r'='
-        self.t_PUNTO = '.'
-        self.t_ID = t_ID  = r'[a-zA-Z_][a-zA-Z0-9_]*'
+t_APARENT = r'\('
+t_CPARENT = r'\('
+t_SUM = r'\+'
+t_RES = r'-'
+t_MULT = r'\*'   
+t_DIV = r'/'
+t_POT = r'\^' 
+t_IGUAL = r'='
+t_PUNTO = r'\.'
+t_ID = t_ID  = r'[a-zA-Z_][a-zA-Z0-9_]*'
+
+def t_NUMERO( t ):
+    r'\d+'
+    try:
+        t.value = int(t.value)
+    except ValueError:
+        print("Integer value too large %d", t.value)
+        t.value = 0
+    return t
+
+def t_error( t ):
+    print("Caracter ilegal '%s'" % t.value[0])
+    t.lexer.skip(1)
 
 
-        
-    def t_NUMERO( self, t ):
-        r'\d+'
-        try:
-            t.value = int(t.value)
-        except ValueError:
-            print("Integer value too large %d", t.value)
-            t.value = 0
-        return t
-
-    def t_error( self, t ):
-        print("Caracter ilegal '%s'" % t.value[0])
-        t.lexer.skip(1)
-
-    def run(self):
-
-        l = Lexico()
-
-        analizador = lex.lex()
-
-        test = 'test/prueba1.txt'
-
-        fp = codecs.open(test, "r", "utf-8")
-        cadena = fp.read()
-        fp.close()
-
-        analizador.input(cadena)
 
 
-        while True:
-            token = analizador.token()
-            if not token: break
-            print(token)
+
